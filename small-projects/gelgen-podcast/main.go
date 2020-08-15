6467e2e3a98fa97a265361d045def8344bf5ef7b
peiyao <peiyaochang@qnap.com>
2020-08-15 16:15:26 +0800

    Implement the GetFeed interface

--- small-projects/gelgen-podcast/main.go
+++ small-projects/gelgen-podcast/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gelgen-podcasr/feeds"
 	"gelgen-podcasr/itunes"
 	"log"
 )
@@ -22,6 +23,20 @@ func main() {
 		log.Printf("Artist: %s", item.ArtistName)
 		log.Printf("Podcast Name: %s", item.TrackName)
 		log.Printf("Feed URL: %s", item.FeedURL)
+
+		// 透過 feed URL 取得一集的 podcast 資訊
+		feed, err := feeds.GetFeed(item.FeedURL)
+		if err != nil {
+			log.Fatal("Error while getting feed: %v", err)
+		}
+
+		for _, pod := range feed.Channel.Item {
+			log.Println("###############")
+			log.Printf("Title: %s", pod.Title)
+			log.Printf("Duration: %s", pod.Duration)
+			log.Printf("Description: %s", pod.Description)
+			log.Printf("URL: %s", pod.Enclosure.URL)
+		}
 		log.Println("---------------------")
 	}
 }
